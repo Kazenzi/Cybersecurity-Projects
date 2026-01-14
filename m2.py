bc61751e165f76df5d39495ea073bd1ce988698f
Kazenzi <jacintakazenzi99@gmail.com>
2026-01-14 21:35:46 +0300

    changes

---
+++ m2.py
@@ -0,0 +1,47 @@
+#funtions  a section of code that can be reused -improve efficiency
+#user defined 
+#def keyword for function
+
+def greetemployee():
+    print("Welcome 2 yes")
+
+greetemployee()
+
+
+# parameter an object used in a function
+# range (start , stop)
+
+def greetemployee(name):
+    print("Welcome 2 yes", name)
+
+greetemployee("jess")
+
+# argument data brought in a funtion when called 
+
+def calculatefails(total, failed):
+    # prevent division by zero
+    if total == 0:
+        return 0
+
+    failure = failed / total
+    return failure
+
+
+# call the function
+result = calculatefails(4, 2)
+
+# display the result
+print("Failure rate:", result)
+ 
+# return retuen information from a function
+
+
+
+print(type ("secuity"))
+print(type(75));
+
+
+names=["jess","francy","anna","kaz"]
+print(sorted(names))
+print(names)
+
---
+++ practice.py
@@ -0,0 +1,31 @@
+print("Hello world!")
+print("I am using Python.")
+# This cell displays all the statements written so far
+
+print("Hello world!")
+print("I am using Python.")
+print("I am a security analyst.")
+print("Python is useful for security!")
+
+# data types
+# string data
+# integer
+# boolean 
+# list data-collection of data 
+# set unordered colection of unique values 
+# tuple data that caanot be changed
+# dictionary data tahta consisnts of one or more key values pairs 
+# variables used later in the code 
+jacy = ' KSX'
+print(jacy)
+
+# type error using the wrong data type
+
+# conditional statements
+# > greater than
+# < less than 
+# >= greater than or equal to 
+# <= less than or equal to
+# != not equal to
+
+# iteratite statements/loops for based on specifi  and while  on a condition
